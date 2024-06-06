af34aa26cc6fbe20cc72d6e79ed1205b64c6c963
MartinSosa <73361548+MartinSosa777@users.noreply.github.com>
2024-06-06 12:09:01 -0300

    Add files via upload

---
+++ intro-web-dev-v2/prueba-intro-web/obtener.js
@@ -0,0 +1,29 @@
+
+
+function obtener() {
+    const usuarios = JSON.parse(localStorage.getItem("usuariosRegistrados"));
+
+    const olUsuarios = document.querySelector("#contenedor-usuarios");
+
+
+    usuarios.forEach(usuario => {
+        olUsuarios.innerHTML += `
+            <li>
+                <ul>
+                    <li>
+                        ${usuario.nombre}
+                    </li>
+                    <li>
+                        ${usuario.email}
+                    </li>
+                    <li>
+                        ${usuario.contrasenia}
+                    </li>
+                </ul>
+            </li>
+        `;
+    });
+}
+
+
+obtener();
