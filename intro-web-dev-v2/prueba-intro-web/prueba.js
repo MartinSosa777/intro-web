dd142feb64796d2d995bc9c6aa5e797f385e386e
pc 2 <>
2024-06-07 12:12:42 -0300

    agregue el required y solucione lo de la consola

--- intro-web-dev-v2/prueba-intro-web/prueba.js
+++ intro-web-dev-v2/prueba-intro-web/prueba.js
@@ -12,4 +12,5 @@ function main() {
     const usuarios = JSON.parse(localStorage.getItem("usuariosRegistrados")) || [];
     usuarios.push(nuevoUsuario)
     localStorage.setItem("usuariosRegistrados", JSON.stringify(usuarios))
+    console.log(usuarios);
 }
